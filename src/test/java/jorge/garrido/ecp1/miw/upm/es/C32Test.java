09d35ea6801320432b1fb2c40e85d10b95634218
Jorge Garrido <torkanwarrior@gmail.com>
2015-09-29 21:08:05 +0200

    C32Test

---
+++ src/test/java/jorge/garrido/ecp1/miw/upm/es/C32Test.java
@@ -0,0 +1,21 @@
+package jorge.garrido.ecp1.miw.upm.es;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class C32Test {
+
+    private C32 c;
+    
+    @Before 
+    public void before(){
+        c = new C32();
+    }
+
+    @Test
+    public void testMA(){
+        assertEquals("mA", c.mA());
+    }
+}
