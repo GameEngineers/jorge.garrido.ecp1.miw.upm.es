a00dc3b434b78e743e812f2081c4fff5d18b6cc4
Jorge Garrido <torkanwarrior@gmail.com>
2015-09-30 20:54:21 +0200

    C52Test

---
+++ src/test/java/jorge/garrido/ecp1/miw/upm/es/C52Test.java
@@ -0,0 +1,22 @@
+package jorge.garrido.ecp1.miw.upm.es;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class C52Test {
+
+    private C52 c;
+    
+    @Before
+    public void before(){
+        c = new C52();
+    }
+    
+    @Test
+    public void testMA(){
+        assertEquals("mA", c.mA());
+    }
+
+}
